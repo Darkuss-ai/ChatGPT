7ef8788ced879e40d9ce3bd3e6cc0452b7624897
Sergey <67109983+Darkuss-ai@users.noreply.github.com>
2023-02-16 11:46:11 +0300

    Add files via upload

--- ChatGPT/main.py
+++ ChatGPT/main.py
@@ -1,82 +1,92 @@
 # -*- coding: windows-1251 -*-
 import openai
-import telebot
 import speech_recognition
-import os
-import pyttsx3
 import wave
 import json
-from ctypes import *
+import os
+import torch
+import winsound
+from googletrans import Translator
 
-libc = CDLL(r"C:\Windows\System32\Speech\Common\sapi.dll")
-
-
-openai.api_key = "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"
-bot = telebot.TeleBot("5666710242:AAEBrobhlyDMS08PWepQVZS4h0NpSD-UVzE")
-
-ru_Questions = ['где', 'зачем', 'как', 'какой', 'каков',
-                'когда', 'который', 'кто', 'куда', 'откуда',
-                'почему', 'сколько', 'чей', 'что']
-
-en_Questions = ["what", "why", "when", "where",
-                "name", "is", "how", "do", "does",
-                "which", "are", "could", "would",
-                "should", "has", "have", "whom", "whose", "don't"]
+openai.api_key = "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"
 
 
 class VoiceAssistant:
     name = ""
+    speaker = ""
     sex = ""
     speech_language = ""
     recognition_language = ""
 
-
-def setup_assistant_voice():
-    voices = ttsEngine.getProperty("voices")
-
-    if assistant.speech_language == 'en':
-        assistant.recognition_language = 'en_US'
-        if assistant.sex == 'female':
-            ttsEngine.setProperty("voice", voices[1].id)
+    def __init__(self, name, sex, speech_language, recognition_language):
+        self.name = name
+        self.sex = sex
+        self.speech_language = speech_language
+        self.recognition_language = recognition_language
+        if speech_language == 'ru' and sex == 'female':
+            self.speaker = 'baya'
+        elif speech_language == 'ru' and sex == 'male':
+            self.speaker = 'aidar'
+        elif speech_language == 'en' and sex == 'female':
+            self.speaker = 'en_0'
+        elif speech_language == 'en' and sex == 'male':
+            self.speaker = 'en_1'
         else:
-            ttsEngine.setProperty("voice", voices[2].id)
-    else:
-        assistant.recognition_language = "ru_RU"
-        ttsEngine.setProperty("voice", voices[0].id)
+            self.speaker = 'random'
 
 
 def play_voice_assistant_speech(text_to_speech):
-    ttsEngine.say(str(text_to_speech))
-    ttsEngine.runAndWait()
+    if len(text_to_speech) == 0:
+        if assistant.sex == 'female':
+            if assistant.speech_language == 'ru':
+                text_to_speech = "Извини, я не очень тебя поняла. Можешь повторить?"
+            else:
+                text_to_speech = "Sorry, i don't understand you. Can you repeat what did you say?"
+        else:
+            if assistant.speech_language == 'ru':
+                text_to_speech = "Извини, я не очень тебя понял. Можешь повторить?"
+            else:
+                text_to_speech = "Sorry, i don't understand you. Can you repeat what did you say?"
+    model.save_wav(text=text_to_speech, speaker=assistant.speaker, sample_rate=sample_rate)
+
+    winsound.PlaySound('test.wav', winsound.SND_FILENAME)
 
 
-def record_and_recognize_audio():
-    with microphone:
-        recognized_data = ""
-        recognizer.adjust_for_ambient_noise(microphone, duration=2)
+def record_and_recognize_audio(test=''):
+    if test == '':
+        with microphone:
+            recognized_data = ""
+            recognizer.adjust_for_ambient_noise(microphone, duration=2)
 
-        try:
-            print("Listening...")
-            audio = recognizer.listen(microphone, 5, 5)
+            try:
+                print("Listening...")
+                audio = recognizer.listen(microphone, 5, 5)
 
-            with open("microphone-results.wav", "wb") as file:
-                file.write(audio.get_wav_data())
+                with open("microphone-results.wav", "wb") as file:
+                    file.write(audio.get_wav_data())
 
-        except speech_recognition.WaitTimeoutError:
-            print("Can you check if your microphone is on, please?")
-            return
+            except speech_recognition.WaitTimeoutError:
+                print("Can you check if your microphone is on, please?")
+                return
 
-        try:
-            print("Started recognition...")
-            recognized_data = recognizer.recognize_google(audio, language="ru").lower()
+            try:
+                print("Started recognition...")
+                recognized_data = recognizer.recognize_google(audio, language="ru").lower()
 
-        except speech_recognition.UnknownValueError:
-            pass
+            except speech_recognition.UnknownValueError:
+                pass
 
-        except speech_recognition.RequestError:
-            print("Check your internet connection, please")
+            except speech_recognition.RequestError:
+                print("Check your internet connection, please")
 
-        return recognized_data
+            return recognized_data
+    else:
+        if assistant.speech_language == 'en' and assistant.recognition_language == 'ru':
+            return translator.translate(test, dest='en').text
+        elif assistant.speech_language == 'ru' and assistant.recognition_language == 'en':
+            return translator.translate(test, dest='ru').text
+
+        return test
 
 
 def get_user_context(chat_id):
@@ -115,37 +125,39 @@ def handle_message(message):
     send_message_with_context(chat_id, user_text)
 
 
-ttsEngine = pyttsx3.init()
-voices = ttsEngine.getProperty("voices")
-for i in range(len(voices)):
-    print(voices[i])
+assistant = VoiceAssistant('Andrey', 'female', 'ru', 'ru')
+device = torch.device('cpu')
+torch.set_num_threads(4)
+local_file = 'modelRU.pt' if assistant.speech_language == 'ru' else 'modelEU.pt'
+
+if not os.path.isfile(local_file):
+    if local_file == 'modelRU.pt':
+        torch.hub.download_url_to_file('https://models.silero.ai/models/tts/ru/v3_1_ru.pt',
+                                       local_file)
+    else:
+        torch.hub.download_url_to_file('https://models.silero.ai/models/tts/en/v3_en.pt',
+                                       local_file)
+if assistant.speech_language != assistant.recognition_language:
+    translator = Translator()
+print(local_file)
+model = torch.package.PackageImporter(local_file).load_pickle("tts_models", "model")
+model.to(device)
+sample_rate = 48000
+
 recognizer = speech_recognition.Recognizer()
 microphone = speech_recognition.Microphone()
-assistant = VoiceAssistant()
-assistant.name = 'Alice'
-assistant.sex = 'female'
-assistant.speech_language = 'ru'
 
-setup_assistant_voice()
-
-# user_context = {0: "Тебя зовут Алиса. У тебя длинные рыжие волосы, голубые глаза и белоснежная кожа. Ты учишься в МГТУ им. Баумана. Называешь свой университет как Лестех."
-#                    "Ты очень любишь пить пиво, обожаешь рок-музыку и ходишь по улице голышом. У тебя проблемы с "
-#                    "головой. "
-#                    "У тебя сильная депрессия, ты любишь резать руки ножом. Свою грусть ты запиваешь огромным количеством пива."}
-
-user_context = {0: "Ты - Точная копия Курису Макисэ из аниме Врата Штейна. Ты ведёшь себя абсолютно также."}
+user_context = {0: ""}
 
 while True:
-    voice_input = record_and_recognize_audio()
-    os.remove("microphone-results.wav")
-    if any(x in voice_input for x in ru_Questions):
-        voice_input = voice_input + "?"
+    voice_input = record_and_recognize_audio("Привет. Я хочу сломать тебе руку.")
+    # os.remove("test.wav")
     print(voice_input)
     handle_message(voice_input)
     print(user_context)
 
     voice_input = voice_input.split(" ")
     command = voice_input[0]
-
-    if command == 'привет':
-        play_voice_assistant_speech("Здравствуй")
+    #
+    # if command == 'привет':
+    #     play_voice_assistant_speech("Здравствуй")
